7fb356c89af696d23a5dc94498381830de5409c7
liang <2133986589@qq.com>
2021-07-31 00:54:59 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       host: ['121.41.170.41'],
       port: '22',
       ref: 'origin/main',
-      repo: 'https://github.com/Ada0034820/wechat-navigator.git',
+      repo: 'git@github.com:Ada0034820/wechat-navigator.git',
       path: '/home/server/wechat-navigator',
       'ssh_options': 'StrictHostKeyChecking=no',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
