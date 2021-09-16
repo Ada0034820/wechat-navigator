f7fe564081dfe8e4d9d3d08543cfe9ef96de2a16
liangjiaxun <liangjiaxun@yidian-inc.com>
2021-09-16 15:02:22 +0800

    update

--- controller/wechat/logins.js
+++ controller/wechat/logins.js
@@ -13,14 +13,15 @@ class LoginHandle extends WechatComponent {
       const code = req.query.code;
       let url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx8e909dc564460200&secret=9c6413b62c2174ef7d9f03e8883c1252&code=${code}&grant_type=authorization_code`;
       request(url, function (error, response, body) {
-        if (response.body["access_token"]) {
-          let scopeUserInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${response.body["access_token"]}&openid=${response.body["openid"]}&lang=zh_CN`;
-          request(scopeUserInfoUrl, function (error, response, body) {
-            res.send(response.body);
-          });
-        } else {
-          res.send(response.body);
-        }
+        // if (response.body["access_token"]) {
+        //   let scopeUserInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${response.body["access_token"]}&openid=${response.body["openid"]}&lang=zh_CN`;
+        //   request(scopeUserInfoUrl, function (error, response, body) {
+        //     res.send(response.body);
+        //   });
+        // } else {
+        //   res.send(response.body);
+        // }
+        res.send(response.body);
       });
     } catch (err) {
       res.send({
