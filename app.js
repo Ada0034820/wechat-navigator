fef906ba2117b321d2c8e8ec930778264f21f51f
liangjiaxun <liangjiaxun@yidian-inc.com>
2021-09-16 11:58:19 +0800

    update

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-const express = require('express')
-const MongoStore = require('connect-mongo');
-const session = require('express-session')
-const config = require('config-lite')(__dirname);
-const router = require('./routes/index.js')
-const db = require('./mongodb/db.js')
-var proxy = require('express-http-proxy');
+const express = require("express");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+const config = require("config-lite")(__dirname);
+const router = require("./routes/index.js");
+const db = require("./mongodb/db.js");
+var proxy = require("express-http-proxy");
 // import config from 'config-lite';
-const app = express()
-const proxyServer = express()
-const port = 3000
-const isdev = process.env.NODE_ENV === 'development'
+const app = express();
+const proxyServer = express();
+const port = 3000;
+const isdev = process.env.NODE_ENV === "development";
 
-app.use(session({
-  name: config.session.name,
-  secret: config.session.secret,
-  resave: true,
-  saveUninitialized: false,
-  cookie: config.session.cookie,
-  store: MongoStore.create({
-    mongoUrl: config.url
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "content-type");
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  next();
+});
+app.use(
+  session({
+    name: config.session.name,
+    secret: config.session.secret,
+    resave: true,
+    saveUninitialized: false,
+    cookie: config.session.cookie,
+    store: MongoStore.create({
+      mongoUrl: config.url,
+    }),
   })
-}))
-proxyServer.use('/proxy', proxy('localhost:3000'));
+);
+proxyServer.use("/proxy", proxy("localhost:3000"));
 router(app);
-proxyServer.use(express.static('./public'));
+proxyServer.use(express.static("./public"));
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 proxyServer.listen(isdev ? 8080 : 80, () => {
-  console.log(`proxyServer listening at http://localhost,mode=${process.env.NODE_ENV}`)
-})
+  console.log(
+    `proxyServer listening at http://localhost,mode=${process.env.NODE_ENV}`
+  );
+});
--- controller/wechat/groups.js
+++ controller/wechat/groups.js
@@ -1,57 +1,58 @@
-const GroupsModel = require('../../models/wechat/Groups')
-const formidable = require("formidable")
-const { v4: uuidv4 } = require('uuid');
-const WechatComponent = require('../../prototype/wechatComponent')
+const GroupsModel = require("../../models/wechat/Groups");
+const formidable = require("formidable");
+const { v4: uuidv4 } = require("uuid");
+const WechatComponent = require("../../prototype/wechatComponent");
 
 class GroupHandle extends WechatComponent {
   constructor() {
-    super()
+    super();
     this.getGroupList = this.getGroupList.bind(this);
   }
   async getGroupList(req, res, next) {
     const type = req.query.type;
     let groupsList;
-    try{
+    try {
       groupsList = await GroupsModel.find({});
       res.send(groupsList);
-    }catch(err) {
+    } catch (err) {
       res.send({
-        name: 'ERROR_DATA',
-        message: '获取数据失败',
+        name: "ERROR_DATA",
+        message: "获取数据失败",
       });
     }
   }
   async addGroup(req, res, next) {
     const form = new formidable.IncomingForm();
     form.parse(req, async (err, fields, files) => {
-      try{
+      console.log(fields);
+      try {
         if (!fields.name) {
-          throw new Error('群名称必填！');
-        }else if(!fields.groupImagePath) {
-          throw new Error('上传图片失败！');
-        }else if(!fields.description) {
-          throw new Error('描述信息不能为空');
+          throw new Error("群名称必填！");
+        } else if (!fields.groupImagePath) {
+          throw new Error("上传图片失败！");
+        } else if (!fields.description) {
+          throw new Error("描述信息不能为空");
         }
         const newGroup = new GroupsModel({
           id: uuidv4(),
           groupImagePath: fields.groupImagePath,
           name: fields.name,
           description: fields.description,
-          uploadTime: new Date().getTime()
+          uploadTime: new Date().getTime(),
         });
         await newGroup.save();
         res.send({
           status: 1,
-          success: '添加成功',
+          success: "添加成功",
         });
-      }catch(err) {
+      } catch (err) {
         res.send({
           status: 0,
-          type: 'ERROR_DATA',
-          message: err.message
-        })
+          type: "ERROR_DATA",
+          message: err.message,
+        });
       }
-    })
+    });
   }
   async editGroup(req, res, next) {
     const form = new formidable.IncomingForm();
@@ -59,67 +60,67 @@ class GroupHandle extends WechatComponent {
       if (err) {
         res.send({
           status: 0,
-          type: 'ERROR_FORM',
-          message: '表单信息错误',
-        })
-        return 
+          type: "ERROR_FORM",
+          message: "表单信息错误",
+        });
+        return;
       }
-      const {groupId} = fields;
-      try{
+      const { groupId } = fields;
+      try {
         if (!groupId) {
-          throw new Error('参数错误');
+          throw new Error("参数错误");
         }
-        const group = await GroupsModel.findOne({id: groupId})
-        if(!group) {
-          throw new Error('没有相关信息');
+        const group = await GroupsModel.findOne({ id: groupId });
+        if (!group) {
+          throw new Error("没有相关信息");
         }
-        let newData = group
-        for(let key in fields) {
-          newData[key] = fields[key] || group[key]
+        let newData = group;
+        for (let key in fields) {
+          newData[key] = fields[key] || group[key];
         }
-        await GroupsModel.findOneAndUpdate({id: groupId}, {$set: newData});
+        await GroupsModel.findOneAndUpdate({ id: groupId }, { $set: newData });
 
         res.send({
           status: 1,
-          success: '修改成功',
-        })
-      }catch(err) {
+          success: "修改成功",
+        });
+      } catch (err) {
         console.log(err.message, err);
         res.send({
           status: 0,
-          type: 'ERROR_UPDATE',
+          type: "ERROR_UPDATE",
           message: err.message,
-        })
+        });
       }
-    })
+    });
   }
   async deleteGroup(req, res, next) {
     const form = new formidable.IncomingForm();
     form.parse(req, async (err, fields, files) => {
-      const id = fields.groupId
-      const group = await GroupsModel.findOne({id});
+      const id = fields.groupId;
+      const group = await GroupsModel.findOne({ id });
       if (!id || !group) {
         res.send({
           status: 0,
-          type: 'ERROR_PARAMS',
-          message: '该信息不存在'
-        })
-        return 
+          type: "ERROR_PARAMS",
+          message: "该信息不存在",
+        });
+        return;
       }
-      try{
-        await group.remove()
+      try {
+        await group.remove();
         res.send({
           status: 1,
-          success: '删除成功'
-        })
-      }catch(err) {
+          success: "删除成功",
+        });
+      } catch (err) {
         res.send({
           status: 0,
-          type: 'DELETE_FAILED',
-          message: err.message
-        })
+          type: "DELETE_FAILED",
+          message: err.message,
+        });
       }
-    })
+    });
   }
 }
-module.exports = new GroupHandle()
+module.exports = new GroupHandle();
